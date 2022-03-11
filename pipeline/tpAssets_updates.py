122ecebb0ab415fe9242d1eee59cdd6e256f7960
Designworks\wwilson <wwilson@designworksgaming.com>
2022-03-11 13:12:49 -0700

    Update tpAssets_updates.py

--- pipeline/tpAssets_updates.py
+++ pipeline/tpAssets_updates.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import os
+import json
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
@@ -29,3 +30,51 @@ if len(sys.argv) >= 2:
         print("url: '%s' error in request." %url)
 else:
     print("necessary args not included.")
+    
+touched = {}
+touchedDirs = []
+for tF in touchedFiles:
+    tF_split = tF.split("/")
+    if len(tF_split) >= 1:
+        full_tF = os.path.join(tF_split[0],tF_split[1])
+        if full_tF not in touchedDirs: touchedDirs.append(full_tF)
+
+for tD in touchedDirs:
+    entry = []
+    for tF in touchedFiles:
+        if tD in tF: entry.append(tF)
+    touched[tD] = entry
+
+for key in touched:
+    print(key)
+    print(touched[key])
+    potential = ""
+    if "games" in key:
+      print("this is a game: '%s'"%key)
+      potential = r"2. Implementation\14. Data\TP_config.json"
+    elif "casino" in key:
+      print("this is a casino: '%s'"%key)
+      potential = r"2. Implementation\14. Data\TP_config.json"
+    else:
+      print("this isn't valid and should be ignored.")
+
+    full_tf = os.path.join(key, potential)
+    print(full_tf)
+    if not os.path.exists(full_tf):
+      print("TP_config file not found.  should make one?")
+    else:
+      print("TP_config file found.  does contain this file in assets?")
+      f = open(full_tf)
+      jsondata = json.load(f)
+      f.close()
+      
+      key = "new_s"%sha
+      jsondata[key] = "new information here"
+      
+      os.remove(full_tf)
+      with open(full_tf, 'w') as out:
+      json.dump(jsondata, out)
+      
+    
+    for tf in touched[key]:
+        print("'%s' exists: %s"%(tf, os.path.exists(tf)))
